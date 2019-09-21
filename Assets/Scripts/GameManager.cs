e5064eb4df31de335c9503418eb7d41656b85123
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-09-20 21:46:52 -0600

    Base setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.PunTutorial {
+  public class GameManager : MonoBehaviourPunCallbacks {
+    #region Photon Callbacks
+    
+    public override void OnleftRoom() {
+      SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+    #region Public Methods
+    
+    public void LeaveRoom() {
+      PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+  }
+
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Com.PunTutorial {
+  public class Launcher : MonoBehaviourPunCallbacks {
+    #region Private Serializable Fields
+
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    [SerializeField]
+    private GameObject controlPanel;
+
+    [SerializeField]
+    private GameObject progressLabel;
+
+    #endregion
+
+    #region Private Fields
+    
+    string gameVersion = "1";
+    
+    #endregion
+    
+    #region MonoBehaviour Callbacks
+
+    void Awake() {
+      PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start() {
+      progressLabel.SetActive(false);
+      controlPanel.SetActive(true);
+    }
+
+    #endregion
+
+    #region Public Methods
+    
+    public void Connect() {
+      progressLabel.SetActive(true);
+      controlPanel.SetActive(false);
+
+      if (PhotonNetwork.IsConnected) {
+        PhotonNetwork.JoinRandomRoom();
+      } else {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+      }
+    }
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
+    public override void OnConnectedToMaster() {
+      Debug.Log("OnConnectedToMaster was called by Pun");
+      PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+      progressLabel.SetActive(false);
+      controlPanel.SetActive(true);
+      Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+      Debug.Log("OnJoinRandomFailed() was called by PUN. No random rooms available, creating one");
+      PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom() {
+      Debug.Log("OnJoinedRoom() called by PUN. Client is in a room");
+    }
+
+    #endregion
+  }
+}
